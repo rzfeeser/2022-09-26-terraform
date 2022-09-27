ae09b524d5166f99b749f903255403b7e3dc42a6
rzfeeser <rzfeeser@example.com>
2022-09-27 05:43:01 +0000

    intro to tfvars

--- docker-challenge/main.tf
+++ docker-challenge/main.tf
@@ -14,12 +14,12 @@ provider "docker" {}
 
 resource "docker_image" "simpleflaskservice" {
   name         = "registry.gitlab.com/alta3research/simpleflaskservice:1.0"
-  keep_locally = true      // keep image after "destroy"
+  keep_locally = true // keep image after "destroy"
 }
 
 resource "docker_container" "simpleflaskservice" {
   image = docker_image.simpleflaskservice.image_id
-  name = var.container_name
+  name  = var.container_name
   ports {
     internal = var.internal_port
     external = var.external_port
